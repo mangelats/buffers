65fe7d72bcd6e6835fae9d7514ddf948e8315881
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-09 01:18:55 +0200

    TCR

--- 2_buffers/src/2_base_buffers/1_inline.rs
+++ 2_buffers/src/2_base_buffers/1_inline.rs
@@ -47,4 +47,15 @@ mod tests {
             assert_eq!(x, r)
         }
     }
+
+    #[test]
+    fn inline_buffer_should_be_able_to_read_multiple_values() {
+        let mut vec = InlineBuffer::<u32, 123>::new();
+        for x in 1..3 {
+            unsafe { vec.mut_ptr(0).write(x) };
+            let r = unsafe { vec.ptr(0).read() };
+
+            assert_eq!(x, r)
+        }
+    }
 }
