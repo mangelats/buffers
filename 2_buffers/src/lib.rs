a6b49a33ca5e6e505b155147e7aaebaa8caa3ac3
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-09 00:41:48 +0200

    TCR

--- 2_buffers/src/lib.rs
+++ 2_buffers/src/lib.rs
@@ -5,10 +5,4 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
