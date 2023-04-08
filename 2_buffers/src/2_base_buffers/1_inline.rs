07b3d0bfe90ffaaceb83dfeb6cd91b0d2d320413
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-09 01:05:01 +0200

    TCR

--- 2_buffers/src/2_base_buffers/1_inline.rs
+++ 2_buffers/src/2_base_buffers/1_inline.rs
@@ -5,5 +5,11 @@ pub struct InlineBuffer<T, const SIZE: usize> {
     array: [MaybeUninit<T>; SIZE],
 }
 
+impl<T, const SIZE: usize> Buffer<T> for InlineBuffer<T, SIZE> {
+    fn capacity(&self) -> usize {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {}
