9a8c14e630b4d593c0ae706bf2ed2a044562f169
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-09 01:06:38 +0200

    TCR

--- 2_buffers/src/2_base_buffers/1_inline.rs
+++ 2_buffers/src/2_base_buffers/1_inline.rs
@@ -13,6 +13,7 @@ impl<T, const SIZE: usize> Buffer<T> for InlineBuffer<T, SIZE> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn inline_buffer_should_have_the_capacity_of_its_size() {}
 }
