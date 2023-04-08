223b9040b603d69ae15b05e46c28cc3a8c40b714
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-09 00:44:59 +0200

    TCR

--- 2_buffers/src/1_traits.rs
+++ 2_buffers/src/1_traits.rs
@@ -1,3 +1,6 @@
+/// Generic representation of a single buffer which contains data.
+///
+/// This is perticularly useful to allow different ways of managing data with a uniform interface.
 pub trait Buffer<T> {}
 
 #[cfg(test)]
