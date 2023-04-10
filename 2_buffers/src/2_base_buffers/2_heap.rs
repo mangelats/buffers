c5d8a461c48fe38e49cdd5bb04412c61ba9f972e
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-10 23:09:33 +0200

    TCR

--- 2_buffers/src/2_base_buffers/2_heap.rs
+++ 2_buffers/src/2_base_buffers/2_heap.rs
@@ -1,5 +1,6 @@
 use std::{marker::PhantomData, ptr::NonNull};
 
+/// Buffer implementation using a heap-allocated continuous array.
 pub struct HeapBuffer<T> {
     ptr: NonNull<T>,
     cap: usize,
