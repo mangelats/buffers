c132c17632d6cb6d58f2f6a0e1312993b543e729
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-13 22:33:21 +0200

    TCR

--- 2_buffers/src/1_interface/2_resize_result.rs
+++ 2_buffers/src/1_interface/2_resize_result.rs
@@ -1,6 +1,11 @@
 /// Errors that may happen when attempting to resize a buffer
 #[derive(Debug, Clone)]
 pub enum ResizeError {
+    /// This buffer will never support this operation
+    /// (eg. trying to resize an inline buffer)
     UnsupportedOperation,
+
+    /// This buffer doesn't support this operation for this type
+    /// (eg. cannot define a memory layout)
     UnsupportedType,
 }
