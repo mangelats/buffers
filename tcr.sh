f59592a36009e609aad00c99c56b5a90d49e6672
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-13 20:49:21 +0200

    TCR

--- tcr.sh
+++ tcr.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 function tcr() {
-    echo "tcr"
     cargo test --workspace \
     && git add -A \
     && git commit -m 'TCR' --quiet \
@@ -9,5 +8,4 @@ function tcr() {
 }
 
 # Do not fail when it's a compiler error
-echo "hi"
 cargo build --workspace && tcr
