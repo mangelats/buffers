e849eddcf9b637b9a12c02c880b451ede2294f6c
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-13 21:04:46 +0200

    TCR

--- tcr.sh
+++ tcr.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 function tcr() {
-    cargo test --workspace --quiet --offline \
+    cargo test --workspace --quiet --offline > /dev/null \
     && git add -A \
     && git commit -m 'TCR' --quiet \
     || git reset --hard --quiet
 }
 
-# Do not fail when it's a compiler error
-cargo test --workspace --no-run --quiet --offline && tcr
+tcr
