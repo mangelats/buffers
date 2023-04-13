4bc1e59b792d0f27779681f25d5f95ec80dbc883
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-13 22:27:23 +0200

    TCR

--- 2_buffers/src/2_base_buffers/2_heap.rs
+++ 2_buffers/src/2_base_buffers/2_heap.rs
@@ -62,7 +62,7 @@ impl<T> Buffer<T> for HeapBuffer<T> {
     unsafe fn try_grow(&mut self, target: usize) -> Result<(), ResizeError> {
         debug_assert!(target > self.cap);
         if self.cap == 0 {
-            Ok(())
+            self.allocate_array_unchecked(target)
         } else {
             Ok(())
         }
