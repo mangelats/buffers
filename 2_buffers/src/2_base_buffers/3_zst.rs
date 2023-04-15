3639ca6cd836b2e2cf538b8bbd0c28b4d9fac215
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 10:46:55 +0200

    TCR

--- 2_buffers/src/2_base_buffers/3_zst.rs
+++ 2_buffers/src/2_base_buffers/3_zst.rs
@@ -28,13 +28,13 @@ impl<T> Buffer<T> for ZstBuffer<T> {
         usize::MAX
     }
 
-    unsafe fn read_value(&self, index: usize) -> T {
+    unsafe fn read_value(&self, _index: usize) -> T {
         // SAFETY: This type has no size. Null should work as well as any other pointer.
         // TODO: adding an intrinsics::assume for the size of T may be worth
         std::ptr::read(std::ptr::NonNull::dangling().as_ptr())
     }
 
-    unsafe fn write_value(&mut self, index: usize, value: T) {}
+    unsafe fn write_value(&mut self, _index: usize, _value: T) {}
 
-    unsafe fn manually_drop(&mut self, index: usize) {}
+    unsafe fn manually_drop(&mut self, _index: usize) {}
 }
