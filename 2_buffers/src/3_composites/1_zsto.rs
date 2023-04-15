2ba8eee8aadcfe8932fa9c5e42bba22e82186364
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 12:01:12 +0200

    TCR

--- 2_buffers/src/3_composites/1_zsto.rs
+++ 2_buffers/src/3_composites/1_zsto.rs
@@ -7,6 +7,7 @@ use super::conditional::{ConditionalBuffer, Selector};
 /// Composite buffer that automatically uses a ZstBuffer when T is a ZST.
 pub type ZstOptBuffer<T, B> = ConditionalBuffer<T, ZstBuffer<T>, B, ZstSelector<T>>;
 
+/// Selector that detects if T is a ZST
 pub struct ZstSelector<T>(PhantomData<T>);
 impl<T> Selector for ZstSelector<T> {
     const SELECT_A: bool = std::mem::size_of::<T>() == 0;
