07d8cc64718c9c872cfe592dcd77ac9e47487a6d
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 10:04:39 +0200

    TCR

--- 1_generic-vec/src/lib.rs
+++ 1_generic-vec/src/lib.rs
@@ -195,7 +195,7 @@ mod tests {
     fn should_panic_if_growing_is_not_allowed() {
         const SIZE: usize = 1;
         let mut vec: Vector<u32, InlineBuffer<u32, SIZE>> = Vector::new();
-        for i in 0..SIZE {
+        for _ in 0..SIZE {
             vec.push(42);
         }
     }
