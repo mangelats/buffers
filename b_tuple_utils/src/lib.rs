0f24bac88689304f8346bee00d774b7f6fe9bdea
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 21:18:27 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -7,30 +7,34 @@ pub trait TypeMap {
 
 pub trait TupleExt: Tuple {
     type MapType<M: TypeMap>;
-}
-impl TupleExt for () {
-    type MapType<M: TypeMap> = ();
+
+    fn map<M>(&self, mapper: M) -> Self::MapType<M>
+    where
+        M: TypeMap;
 }
 
-macro_rules! impl_tuple_map {
-    (() -> ($($body:path,)*)) => { ($($body),*, ) };
-    (($t:ident,$($rest:ident,)*) -> ($($body:path,)*)) => {
-        impl_tuple_map!{ ($($rest,)*) -> (M::Output<$t>, $($body,)*) }
-    };
-}
-macro_rules! impl_tuple_ext_one {
-    ($($t:ident),+) => {
-        impl<$($t),+> TupleExt for ($($t),+,) {
-            type MapType<M: TypeMap> = impl_tuple_map!(($($t,)+) -> ());
-        }
-    };
-}
-macro_rules! impl_tuple_ext {
-    () => {};
-    ($t:ident, $($rest:ident),+) => {
-        impl_tuple_ext_one! {$t, $($rest),+}
-        impl_tuple_ext_one! {$($rest),+}
-    };
-}
+// impl TupleExt for () {
+//     type MapType<M: TypeMap> = ();
+// }
+// macro_rules! impl_tuple_map {
+//     (() -> ($($body:path,)*)) => { ($($body),*, ) };
+//     (($t:ident,$($rest:ident,)*) -> ($($body:path,)*)) => {
+//         impl_tuple_map!{ ($($rest,)*) -> (M::Output<$t>, $($body,)*) }
+//     };
+// }
+// macro_rules! impl_tuple_ext_one {
+//     ($($t:ident),+) => {
+//         impl<$($t),+> TupleExt for ($($t),+,) {
+//             type MapType<M: TypeMap> = impl_tuple_map!(($($t,)+) -> ());
+//         }
+//     };
+// }
+// macro_rules! impl_tuple_ext {
+//     () => {};
+//     ($t:ident, $($rest:ident),+) => {
+//         impl_tuple_ext_one! {$t, $($rest),+}
+//         impl_tuple_ext_one! {$($rest),+}
+//     };
+// }
 
-impl_tuple_ext! {T0, T1, T2, T3, T4}
+// impl_tuple_ext! {T0, T1, T2, T3, T4}
