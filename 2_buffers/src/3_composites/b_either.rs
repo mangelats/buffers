8f2c18d241341ce0c36d4b52a9c621de35f2f76e
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 12:43:16 +0200

    TCR

--- 2_buffers/src/3_composites/b_either.rs
+++ 2_buffers/src/3_composites/b_either.rs
@@ -2,9 +2,10 @@ use std::marker::PhantomData;
 
 use crate::interface::Buffer;
 
-pub enum Never {}
 pub enum EitherBuffer<T, A: Buffer<T>, B: Buffer<T>> {
     First(A),
     Second(B),
     _InternalMarker(Never, PhantomData<T>),
 }
+
+pub enum Never {}
