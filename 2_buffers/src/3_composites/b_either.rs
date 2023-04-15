99138021a0876de1f2179acd671f2f06bc6b81b4
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 12:45:06 +0200

    TCR

--- 2_buffers/src/3_composites/b_either.rs
+++ 2_buffers/src/3_composites/b_either.rs
@@ -3,9 +3,17 @@ use std::marker::PhantomData;
 use crate::interface::Buffer;
 
 pub enum EitherBuffer<T, A: Buffer<T>, B: Buffer<T>> {
+    /// First option (A buffer)
     First(A),
+    /// Second option (B buffer)
     Second(B),
+
+    /// Internal option that never can be selected which holds PhantomData to T
     _InternalMarker(Never, PhantomData<T>),
 }
 
+/// A type that can never exist.
+///
+/// Equivalent to the never type (!) which is experimental
+/// (see issue #35121 <https://github.com/rust-lang/rust/issues/35121>).
 pub enum Never {}
