f09ff9cd862d778bf1091f528e05b46aa7e98c55
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 11:44:52 +0200

    TCR

--- 2_buffers/src/3_composites/a_conditional.rs
+++ 2_buffers/src/3_composites/a_conditional.rs
@@ -12,18 +12,34 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
     for ConditionalBuffer<T, A, B, SELECT_A>
 {
     fn capacity(&self) -> usize {
-        todo!()
+        if SELECT_A {
+            self.a.capacity()
+        } else {
+            self.b.capacity()
+        }
     }
 
     unsafe fn read_value(&self, index: usize) -> T {
-        todo!()
+        if SELECT_A {
+            self.a.read_value(index)
+        } else {
+            self.b.read_value(index)
+        }
     }
 
     unsafe fn write_value(&mut self, index: usize, value: T) {
-        todo!()
+        if SELECT_A {
+            self.a.write_value(index, value)
+        } else {
+            self.b.write_value(index, value)
+        }
     }
 
     unsafe fn manually_drop(&mut self, index: usize) {
-        todo!()
+        if SELECT_A {
+            self.a.manually_drop(index)
+        } else {
+            self.b.manually_drop(index)
+        }
     }
 }
