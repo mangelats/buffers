dba6758e9a8a857f73c8ad17c1db2206d14da8c1
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 16:56:23 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -17,10 +17,10 @@ pub trait TupleExt: Tuple {
         Self: 'a;
     fn as_ref(&self) -> Self::Ref<'_>;
 
-    type MutRef<'a>
-    where
-        Self: 'a;
-    fn as_mut_ref(&mut self) -> Self::MutRef<'_>;
+    // type MutRef<'a>
+    // where
+    //     Self: 'a;
+    // fn as_mut_ref(&mut self) -> Self::MutRef<'_>;
 
     // type ConstPtr;
     // fn as_ptr(*const self) -> ConstPtr;
@@ -33,33 +33,38 @@ pub trait TupleExt: Tuple {
     // fn reduce<R>(self, initial: R, r: Reducer<R, Self>) -> R;
 }
 
-// impl<T0, T1, T2> TupleExt for (T1, T2, T3) {
-//     type Ref<'a> = (&'a T1, &'a T2, &'a T3);
-//     fn as_ref(&self) -> Ref<'_> {
-//         (&self.0, &self.1, &self.2)
-//     }
+impl<T0, T1, T2> TupleExt for (T0, T1, T2) {
+    type Ref<'a> = (&'a T0, &'a T1, &'a T2)
+    where
+        Self: 'a,
+        T0: 'a,
+        T1: 'a,
+        T2: 'a;
+    fn as_ref(&self) -> Self::Ref<'_> {
+        (&self.0, &self.1, &self.2)
+    }
 
-//     type MutRef<'a> = (&'a mut T1, &'a mut T2, &'a mut T3);
-//     fn as_mut_ref(&mut self) -> MutRef<'_> {
-//         (&mut self.0, &mut self.1, &mut self.2)
-//     }
+    //     type MutRef<'a> = (&'a mut T1, &'a mut T2, &'a mut T3);
+    //     fn as_mut_ref(&mut self) -> MutRef<'_> {
+    //         (&mut self.0, &mut self.1, &mut self.2)
+    //     }
 
-//     type ConstPtr = (*const T0, *const T1, *const T2);
-//     fn as_ptr(*const self) -> ConstPtr {
-//         (&*self.0, &*self.1, &*self.2)
-//     }
+    //     type ConstPtr = (*const T0, *const T1, *const T2);
+    //     fn as_ptr(*const self) -> ConstPtr {
+    //         (&*self.0, &*self.1, &*self.2)
+    //     }
 
-//     type MutPtr = (*mut T0, *mut T1, *mut T2);
-//     fn as_mut_ptr(*mut self) -> MutPtr {
-//         (&mut *self.0, &mut *self.1, &mut *self.2)
-//     }
+    //     type MutPtr = (*mut T0, *mut T1, *mut T2);
+    //     fn as_mut_ptr(*mut self) -> MutPtr {
+    //         (&mut *self.0, &mut *self.1, &mut *self.2)
+    //     }
 
-//     type Map<M: TypeMap> = (M::Output<T0>, M::Output<T1>, M::Output<T2>);
+    //     type Map<M: TypeMap> = (M::Output<T0>, M::Output<T1>, M::Output<T2>);
 
-//     fn reduce<R>(self, initial: R, r: Reducer<R, Self>) -> R {
-//         initial
-//     }
-// }
+    //     fn reduce<R>(self, initial: R, r: Reducer<R, Self>) -> R {
+    //         initial
+    //     }
+}
 
 // impl<T0, T1, T2> Includes<T0> for (T0, T1, T2) {}
 // impl<T0, T1, T2> Includes<T1> for (T0, T1, T2) {}
