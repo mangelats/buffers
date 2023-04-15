f40ecf8587be96a4c0a1a0759742c9660d8e52e9
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 17:50:15 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -19,30 +19,30 @@ pub trait TupleExt: Tuple {
 //     type Map<M: TypeMap> = (M::Output<T0>, M::Output<T1>, M::Output<T2>);
 // }
 macro_rules! join_tuple {
-    ($t:path, ($($rest:path),*)) => {
+    ($t:path, ($($rest:path),*,?)) => {
         ($t, $($rest),*)
     }
 }
 macro_rules! impl_tuple_map {
     () => {()};
 
-    (($t:ident)) => {
-        (M::Output<$t>)
+    ($t:ident) => {
+        (M::Output<$t>,)
     };
-    (($t:ident, $($rest:ident),+)) => {
-        join_tuple!($t, impl_tuple_map!{$($rest),+})
+    ($t:ident, $($rest:ident),+) => {
+        join_tuple!(M::Output<$t>, impl_tuple_map!($($rest),+))
     };
 }
 macro_rules! impl_tuple_ext {
     () => {};
-    ($($t:ident),+) => {
-        impl<$($t),+> TupleExt for ($($t),+) {
-            type Map<M: TypeMap> = ();
+    ($($t:ident),*) => {
+        impl<$($t),*> TupleExt for ($($t),*,) {
+            type Map<M: TypeMap> = impl_tuple_map!($($t),*);
         }
     };
 }
 
-impl_tuple_ext! {T0, T1}
+impl_tuple_ext! {T0}
 
 // impl<T0, T1, T2> Includes<T0> for (T0, T1, T2<)> {}
 // impl<T0, T1, T2> Includes<T1> for (T0, T1, T2) {}
