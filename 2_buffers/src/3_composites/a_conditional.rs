97ed2e0af8a70278983535a63da1dc6452c3511a
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 11:57:05 +0200

    TCR

--- 2_buffers/src/3_composites/a_conditional.rs
+++ 2_buffers/src/3_composites/a_conditional.rs
@@ -10,13 +10,13 @@ pub trait Selector {
 ///
 /// Note that this uses both buffers but only uses one. This may be able to change
 /// with generic const expressions.
-pub struct ConditionalBuffer<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> {
+pub struct ConditionalBuffer<T, A: Buffer<T>, B: Buffer<T>, S: Selector> {
     a: A,
     b: B,
-    _m: PhantomData<T>,
+    _m: PhantomData<(T, S)>,
 }
 
-impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> ConditionalBuffer<T, A, B, SELECT_A> {
+impl<T, A: Buffer<T>, B: Buffer<T>, S: Selector> ConditionalBuffer<T, A, B, S> {
     pub fn new(a: A, b: B) -> Self {
         Self {
             a,
@@ -26,19 +26,17 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> ConditionalBuffer<T, A
     }
 }
 
-impl<T, A: Buffer<T> + Default, B: Buffer<T> + Default, const SELECT_A: bool> Default
-    for ConditionalBuffer<T, A, B, SELECT_A>
+impl<T, A: Buffer<T> + Default, B: Buffer<T> + Default, S: Selector> Default
+    for ConditionalBuffer<T, A, B, S>
 {
     fn default() -> Self {
         Self::new(Default::default(), Default::default())
     }
 }
 
-impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
-    for ConditionalBuffer<T, A, B, SELECT_A>
-{
+impl<T, A: Buffer<T>, B: Buffer<T>, S: Selector> Buffer<T> for ConditionalBuffer<T, A, B, S> {
     fn capacity(&self) -> usize {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.capacity()
         } else {
             self.b.capacity()
@@ -46,7 +44,7 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
     }
 
     unsafe fn read_value(&self, index: usize) -> T {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.read_value(index)
         } else {
             self.b.read_value(index)
@@ -54,7 +52,7 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
     }
 
     unsafe fn write_value(&mut self, index: usize, value: T) {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.write_value(index, value)
         } else {
             self.b.write_value(index, value)
@@ -62,7 +60,7 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
     }
 
     unsafe fn manually_drop(&mut self, index: usize) {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.manually_drop(index)
         } else {
             self.b.manually_drop(index)
@@ -70,14 +68,14 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
     }
 
     unsafe fn manually_drop_range(&mut self, values_range: Range<usize>) {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.manually_drop_range(values_range)
         } else {
             self.b.manually_drop_range(values_range)
         }
     }
     unsafe fn try_grow(&mut self, target: usize) -> Result<(), ResizeError> {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.try_grow(target)
         } else {
             self.b.try_grow(target)
@@ -85,7 +83,7 @@ impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
     }
 
     unsafe fn try_shrink(&mut self, target: usize) -> Result<(), ResizeError> {
-        if SELECT_A {
+        if S::SELECT_A {
             self.a.try_shrink(target)
         } else {
             self.b.try_shrink(target)
