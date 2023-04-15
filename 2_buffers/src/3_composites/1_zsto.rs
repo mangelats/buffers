01fbc8207154a6febedaac390ac70cd127bba813
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 10:58:30 +0200

    TCR

--- 2_buffers/src/3_composites/1_zsto.rs
+++ 2_buffers/src/3_composites/1_zsto.rs
@@ -1,2 +1,9 @@
+use std::marker::PhantomData;
+
+use crate::interface::Buffer;
+
 /// Composite buffer that automatically uses a ZstBuffer when T is a ZST.
-pub struct ZstOptBuffer {}
+pub struct ZstOptBuffer<T, B: Buffer<T>> {
+    child: B,
+    _m: PhantomData<T>,
+}
