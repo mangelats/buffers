d8186b4de9aba7096e03ed953dec985d9598a789
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 11:58:21 +0200

    TCR

--- 2_buffers/src/3_composites/1_zsto.rs
+++ 2_buffers/src/3_composites/1_zsto.rs
@@ -2,8 +2,15 @@ use std::marker::PhantomData;
 
 use crate::interface::Buffer;
 
+use super::conditional::Selector;
+
 /// Composite buffer that automatically uses a ZstBuffer when T is a ZST.
 pub struct ZstOptBuffer<T, B: Buffer<T>> {
     child: B,
     _m: PhantomData<T>,
+}
+
+struct ZstSelector<T>(PhantomData<T>);
+impl<T> Selector for ZstSelector<T> {
+    const SELECT_A: bool = std::mem::size_of::<T>() == 0;
 }
