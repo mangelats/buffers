d77fa909c5d053d08012da049c5bd677590a88f6
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 12:45:59 +0200

    TCR

--- 2_buffers/src/3_composites/b_either.rs
+++ 2_buffers/src/3_composites/b_either.rs
@@ -12,6 +12,24 @@ pub enum EitherBuffer<T, A: Buffer<T>, B: Buffer<T>> {
     _InternalMarker(Never, PhantomData<T>),
 }
 
+impl<T, A: Buffer<T>, B: Buffer<T>> Buffer<T> for EitherBuffer<T, A, B> {
+    fn capacity(&self) -> usize {
+        todo!()
+    }
+
+    unsafe fn read_value(&self, index: usize) -> T {
+        todo!()
+    }
+
+    unsafe fn write_value(&mut self, index: usize, value: T) {
+        todo!()
+    }
+
+    unsafe fn manually_drop(&mut self, index: usize) {
+        todo!()
+    }
+}
+
 /// A type that can never exist.
 ///
 /// Equivalent to the never type (!) which is experimental
