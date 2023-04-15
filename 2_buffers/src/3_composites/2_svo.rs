e073cce5502621bd79007d6c1c0c5bdfd29d0c27
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 12:31:37 +0200

    TCR

--- 2_buffers/src/3_composites/2_svo.rs
+++ 2_buffers/src/3_composites/2_svo.rs
@@ -4,3 +4,21 @@ pub struct SvoBuffer<T, B: Buffer<T>, const SMALL_SIZE: usize> {
     small: InlineBuffer<T, SMALL_SIZE>,
     big: B,
 }
+
+impl<T, B: Buffer<T>, const SMALL_SIZE: usize> Buffer<T> for SvoBuffer<T, B, SMALL_SIZE> {
+    fn capacity(&self) -> usize {
+        todo!()
+    }
+
+    unsafe fn read_value(&self, index: usize) -> T {
+        todo!()
+    }
+
+    unsafe fn write_value(&mut self, index: usize, value: T) {
+        todo!()
+    }
+
+    unsafe fn manually_drop(&mut self, index: usize) {
+        todo!()
+    }
+}
