8da5daccbde7fa466305cb055522fc0b3372a786
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 15:14:22 +0200

    TCR

--- 2_buffers/src/2_base_buffers/4_allocator.rs
+++ 2_buffers/src/2_base_buffers/4_allocator.rs
@@ -32,6 +32,16 @@ impl<T, A: Allocator> AllocatorBuffer<T, A> {
         }
     }
 
+    /// Get a constant pointer to the specified index
+    pub unsafe fn ptr(&self, index: usize) -> *const T {
+        self.ptr.as_ptr().add(index)
+    }
+
+    /// Get a mutable pointer to the specified index
+    pub unsafe fn mut_ptr(&mut self, index: usize) -> *mut T {
+        self.ptr.as_ptr().add(index)
+    }
+
     /// Internal function that sets the capacity and raw buffer pointer
     fn update_buffer(&mut self, ptr: NonNull<T>, cap: usize) {
         self.cap = cap;
@@ -45,11 +55,11 @@ impl<T, A: Allocator> Buffer<T> for AllocatorBuffer<T, A> {
     }
 
     unsafe fn read_value(&self, index: usize) -> T {
-        todo!()
+        std::ptr::read(self.ptr(index))
     }
 
     unsafe fn write_value(&mut self, index: usize, value: T) {
-        todo!()
+        std::ptr::write(self.mut_ptr(index), value)
     }
 
     unsafe fn manually_drop(&mut self, index: usize) {
