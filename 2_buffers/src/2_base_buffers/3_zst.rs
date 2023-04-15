ff0c9e755acf61c0eef086d8b88110ed89dd4e9d
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 10:38:13 +0200

    TCR

--- 2_buffers/src/2_base_buffers/3_zst.rs
+++ 2_buffers/src/2_base_buffers/3_zst.rs
@@ -5,3 +5,21 @@ use crate::interface::Buffer;
 pub struct ZstBuffer<T> {
     _m: PhantomData<T>,
 }
+
+impl<T> Buffer<T> for ZstBuffer<T> {
+    fn capacity(&self) -> usize {
+        todo!()
+    }
+
+    unsafe fn read_value(&self, index: usize) -> T {
+        todo!()
+    }
+
+    unsafe fn write_value(&mut self, index: usize, value: T) {
+        todo!()
+    }
+
+    unsafe fn manually_drop(&mut self, index: usize) {
+        todo!()
+    }
+}
