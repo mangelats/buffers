eb94f2cf8c7ea9340f8d6332fef119839fa1c007
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 12:25:15 +0200

    TCR

---
+++ 2_buffers/src/3_composites/2_svo.rs
@@ -0,0 +1,8 @@
+use std::marker::PhantomData;
+
+use crate::{base_buffers::inline::InlineBuffer, interface::Buffer};
+
+pub struct SvoBuffer<T, B: Buffer<T>, const SMALL_SIZE: usize> {
+    small: InlineBuffer<T, SMALL_SIZE>,
+    big: B,
+}
