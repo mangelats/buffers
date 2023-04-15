a2c9792ded1d109172da1875fc92edab0b9e6e5b
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 16:31:50 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -3,8 +3,10 @@ use std::marker::Tuple;
 
 pub trait TupleExt: Tuple {
     type Ref<'a>;
-
     fn as_ref<'a>(&'a self) -> Ref<'a>;
+
+    type MutRef<'a>;
+    fn as_mut_ref(&mut self) -> MutRef<'_>;
 }
 
 impl<T0, T1, T2> TupleExt for (T1, T2, T3) {
@@ -12,5 +14,11 @@ impl<T0, T1, T2> TupleExt for (T1, T2, T3) {
 
     fn as_ref(&self) -> Ref<'_> {
         (&self.0, &self.1, &self.2)
+    }
+
+    type MutRef<'a> = (&'a mut T1, &'a mut T2, &'a mut T3);
+
+    fn as_mut_ref(&mut self) -> MutRef<'_> {
+        (&mut self.0, &mut self.1, &mut self.2)
     }
 }
