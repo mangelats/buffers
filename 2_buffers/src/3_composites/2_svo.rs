83ba8e308f628c9e36d28bbfb92fa9933fc521a3
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 13:06:57 +0200

    TCR

--- 2_buffers/src/3_composites/2_svo.rs
+++ 2_buffers/src/3_composites/2_svo.rs
@@ -21,6 +21,7 @@ impl<T, B: Buffer<T> + Default, const SMALL_SIZE: usize> SvoBuffer<T, B, SMALL_S
         for index in 0..current_buf.capacity() {
             new_buf.write_value(index, current_buf.read_value(index))
         }
+        self.inner = EitherBuffer::Second(new_buf);
         Ok(())
     }
 }
