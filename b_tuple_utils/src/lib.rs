bc602665a03fc7459d1f145fa41c167380809a61
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 16:33:48 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -7,6 +7,9 @@ pub trait TupleExt: Tuple {
 
     type MutRef<'a>;
     fn as_mut_ref(&mut self) -> MutRef<'_>;
+
+    type ConstPtr;
+    fn as_ptr(*const self) -> ConstPtr;
 }
 
 impl<T0, T1, T2> TupleExt for (T1, T2, T3) {
@@ -18,5 +21,10 @@ impl<T0, T1, T2> TupleExt for (T1, T2, T3) {
     type MutRef<'a> = (&'a mut T1, &'a mut T2, &'a mut T3);
     fn as_mut_ref(&mut self) -> MutRef<'_> {
         (&mut self.0, &mut self.1, &mut self.2)
+    }
+
+    type ConstPtr = (*const T0, *const T1, *const T2);
+    fn as_ptr(*const self) -> ConstPtr {
+        (&*self.0, &*self.1, &*self.2)
     }
 }
