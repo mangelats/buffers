0ae5cc6ff1e899cdc3100970202381b0b3c69cb7
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 17:18:54 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -12,63 +12,11 @@ pub trait TypeMap {
 // }
 
 pub trait TupleExt: Tuple {
-    type Ref<'a>
-    where
-        Self: 'a;
-    fn as_ref(&self) -> Self::Ref<'_>;
-
-    type MutRef<'a>
-    where
-        Self: 'a;
-    fn as_mut_ref(&mut self) -> Self::MutRef<'_>;
-
-    // type ConstPtr;
-    // fn as_ptr(ptr: *const Self) -> Self::ConstPtr;
-
-    // type MutPtr;
-    // fn as_mut_ptr(*mut self) -> MutPtr;
-
-    // type Map<M: TypeMap>;
-
-    // fn reduce<R>(self, initial: R, r: Reducer<R, Self>) -> R;
+    type Map<M: TypeMap>;
 }
 
 impl<T0, T1, T2> TupleExt for (T0, T1, T2) {
-    type Ref<'a> = (&'a T0, &'a T1, &'a T2)
-    where
-        Self: 'a,
-        T0: 'a,
-        T1: 'a,
-        T2: 'a;
-    fn as_ref<'a>(&'a self) -> Self::Ref<'a> {
-        (&self.0, &self.1, &self.2)
-    }
-
-    type MutRef<'a> = (&'a mut T0, &'a mut T1, &'a mut T2)
-    where
-        Self: 'a,
-        T0: 'a,
-        T1: 'a,
-        T2: 'a;
-    fn as_mut_ref<'a>(&'a mut self) -> Self::MutRef<'a> {
-        (&mut self.0, &mut self.1, &mut self.2)
-    }
-
-    // type ConstPtr = (*const T0, *const T1, *const T2);
-    // fn as_ptr(ptr: *const Self) -> Self::ConstPtr {
-    //     (&r.0 as *const T0, &r.1 as *const T1, &r.2 as *const T2)
-    // }
-
-    //     type MutPtr = (*mut T0, *mut T1, *mut T2);
-    //     fn as_mut_ptr(*mut self) -> MutPtr {
-    //         (&mut *self.0, &mut *self.1, &mut *self.2)
-    //     }
-
-    //     type Map<M: TypeMap> = (M::Output<T0>, M::Output<T1>, M::Output<T2>);
-
-    //     fn reduce<R>(self, initial: R, r: Reducer<R, Self>) -> R {
-    //         initial
-    //     }
+    type Map<M: TypeMap> = (M::Output<T0>, M::Output<T1>, M::Output<T2>);
 }
 
 // impl<T0, T1, T2> Includes<T0> for (T0, T1, T2) {}
