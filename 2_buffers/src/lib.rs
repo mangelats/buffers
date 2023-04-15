e8af40d7f7a4a8ba93f45ade9e9a595b071055b3
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 14:58:41 +0200

    TCR

--- 2_buffers/src/lib.rs
+++ 2_buffers/src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(dropck_eyepatch)]
 #![feature(maybe_uninit_uninit_array)]
 
+use base_buffers::heap::HeapBuffer;
+use composites::{svo::SvoBuffer, zsto::ZstOptBuffer};
+
 #[path = "1_interface/_mod.rs"]
 pub mod interface;
 
@@ -9,3 +12,5 @@ pub mod base_buffers;
 
 #[path = "3_composites/_mod.rs"]
 pub mod composites;
+
+pub type DefaultBuffer<T> = ZstOptBuffer<T, SvoBuffer<T, HeapBuffer<T>, 256>>;
