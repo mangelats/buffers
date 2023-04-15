291d183508e06d44600868756632870c3adf85be
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 16:30:56 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -10,7 +10,7 @@ pub trait TupleExt: Tuple {
 impl<T0, T1, T2> TupleExt for (T1, T2, T3) {
     type Ref<'a> = (&'a T1, &'a T2, &'a T3);
 
-    fn as_ref<'a>(&'a self) -> Ref<'a> {
+    fn as_ref(&self) -> Ref<'_> {
         (&self.0, &self.1, &self.2)
     }
 }
