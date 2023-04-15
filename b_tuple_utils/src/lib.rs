75ce5740fec0df1dec5ae41f58ea29d2ede19c00
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 21:11:56 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -5,49 +5,32 @@ pub trait TypeMap {
     type Output<T>;
 }
 
-// pub trait<U> Includes<U>: Tuple {}
-
-// pub trait Reducer<R, T: Tuple> {
-//     fn reduce<U>(current: R, value: U) -> R where T: Includes<U>;
-// }
-
 pub trait TupleExt: Tuple {
     type Map<M: TypeMap>;
 }
-
-// impl<T0, T1, T2> TupleExt for (T0, T1, T2) {
-//     type Map<M: TypeMap> = (M::Output<T0>, M::Output<T1>, M::Output<T2>);
-// }
-macro_rules! join_tuple {
-    ($t:path, ($($rest:path),*,?)) => {
-        ($t, $($rest),*)
-    }
+impl TupleExt for () {
+    type Map<M: TypeMap> = ();
 }
-macro_rules! impl_tuple_map {
-    () => {()};
 
-    ($t:ident) => {
-        (M::Output<$t>,)
+macro_rules! impl_tuple_map {
+    (() -> ($($body:path,)*)) => { ($($body),*, ) };
+    (($t:ident,$($rest:ident,)*) -> ($($body:path,)*)) => {
+        impl_tuple_map!{ ($($rest,)*) -> (M::Output<$t>, $($body,)*) }
     };
-    ($t:ident, $($rest:ident),+) => {
-        join_tuple!(M::Output<$t>, impl_tuple_map!($($rest),+))
+}
+macro_rules! impl_tuple_ext_one {
+    ($($t:ident),+) => {
+        impl<$($t),+> TupleExt for ($($t),+,) {
+            type Map<M: TypeMap> = impl_tuple_map!(($($t,)+) -> ());
+        }
     };
 }
 macro_rules! impl_tuple_ext {
     () => {};
-    ($($t:ident),*) => {
-        impl<$($t),*> TupleExt for ($($t),*,) {
-            type Map<M: TypeMap> = impl_tuple_map!($($t),*);
-        }
+    ($t:ident, $($rest:ident),+) => {
+        impl_tuple_ext_one! {$t, $($rest),+}
+        impl_tuple_ext_one! {$($rest),+}
     };
 }
 
-impl_tuple_ext! {T0}
-
-// impl<T0, T1, T2> Includes<T0> for (T0, T1, T2<)> {}
-// impl<T0, T1, T2> Includes<T1> for (T0, T1, T2) {}
-// impl<T0, T1, T2> Includes<T2> for (T0, T1, T2) {}
-
-// pub trait Reducer<R, T: Tuple> {
-//     fn reduce<U>(current: R, value: U) -> R where T: Includes<U>;
-// }
+impl_tuple_ext! {T0, T1, T2, T3, T4}
