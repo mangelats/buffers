a6d2862c8eea34cd7f2b98e185db145668b424e7
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 13:03:54 +0200

    TCR

--- 2_buffers/src/3_composites/2_svo.rs
+++ 2_buffers/src/3_composites/2_svo.rs
@@ -13,8 +13,11 @@ pub struct SvoBuffer<T, B: Buffer<T> + Default, const SMALL_SIZE: usize> {
 
 impl<T, B: Buffer<T> + Default, const SMALL_SIZE: usize> SvoBuffer<T, B, SMALL_SIZE> {
     unsafe fn move_into_big(&mut self, target: usize) -> Result<(), ResizeError> {
+        let previous_ = self.capacity();
+        let EitherBuffer::First(ref current_buf) = self.inner else { unreachable!() };
         let mut new_buf: B = Default::default();
         new_buf.try_grow(target)?;
+
         Ok(())
     }
 }
