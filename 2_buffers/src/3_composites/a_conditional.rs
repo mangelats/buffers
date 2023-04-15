c5e80dedf7aec78292c24a4c1d06f46038a1407a
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-15 11:43:43 +0200

    TCR

--- 2_buffers/src/3_composites/a_conditional.rs
+++ 2_buffers/src/3_composites/a_conditional.rs
@@ -7,3 +7,23 @@ pub struct ConditionalBuffer<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool
     b: B,
     _m: PhantomData<T>,
 }
+
+impl<T, A: Buffer<T>, B: Buffer<T>, const SELECT_A: bool> Buffer<T>
+    for ConditionalBuffer<T, A, B, SELECT_A>
+{
+    fn capacity(&self) -> usize {
+        todo!()
+    }
+
+    unsafe fn read_value(&self, index: usize) -> T {
+        todo!()
+    }
+
+    unsafe fn write_value(&mut self, index: usize, value: T) {
+        todo!()
+    }
+
+    unsafe fn manually_drop(&mut self, index: usize) {
+        todo!()
+    }
+}
