1e619ea9fa4ce7d411b75fd87f0beec0c64f1dcc
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-17 23:04:35 +0200

    TCR

--- c_tuple_utils_proc_macros/src/lib.rs
+++ c_tuple_utils_proc_macros/src/lib.rs
@@ -7,10 +7,12 @@ use quote::TokenStreamExt;
 #[proc_macro]
 pub fn tuple_ext_impl(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut generated = TokenStream::new();
-    generated.append_all(inner());
+    for i in 0..12 {
+        generated.append_all(generate_for_size(i));
+    }
     generated.into()
 }
 
-fn inner() -> TokenStream {
+fn generate_for_size(i: usize) -> TokenStream {
     quote!(let _ = "hi")
 }
