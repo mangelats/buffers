120165d53d970da8a8c569505fdaab39bd546e2d
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-17 23:02:06 +0200

    TCR

--- c_tuple_utils_proc_macros/src/lib.rs
+++ c_tuple_utils_proc_macros/src/lib.rs
@@ -1,12 +1,10 @@
-#[macro_use]
 extern crate quote;
 
-use proc_macro::Ident;
 use proc_macro2::TokenStream;
 use quote::quote;
 
 #[proc_macro]
-pub fn tuple_ext_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn tuple_ext_impl(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     inner().into()
 }
 
