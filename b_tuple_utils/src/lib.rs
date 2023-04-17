926b17ef59895b38e73516216cfe3dafee1c99d9
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-17 23:22:04 +0200

    TCR

--- b_tuple_utils/src/lib.rs
+++ b_tuple_utils/src/lib.rs
@@ -3,4 +3,9 @@ use tuple_utils_proc_macros::tuple_ext_impl;
 tuple_ext_impl!();
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_be_able_to_map_values() {}
+}
