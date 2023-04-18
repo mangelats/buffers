82b31625389b47e2b3e05694739533838c5d01ea
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-18 18:25:36 +0200

    TCR

--- c_tuple_utils_proc_macros/src/lib.rs
+++ c_tuple_utils_proc_macros/src/lib.rs
@@ -27,6 +27,7 @@ pub fn tuple_ext_impl(_input: proc_macro::TokenStream) -> proc_macro::TokenStrea
 }
 
 fn generate_for_size(i: usize) -> TokenStream {
+    let fields: Vec<_> = (0..=i).into_iter().map(number_ident).collect();
     let names: Vec<_> = (0..=i).into_iter().map(type_ident).collect();
     quote!(
         impl< #(#names, )* > Sealed for ( #(#names, )* ) {}
