567f53b0e355079c3569428939ccb87a8d2e436d
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-18 18:37:25 +0200

    TCR

--- c_tuple_utils_proc_macros/src/lib.rs
+++ c_tuple_utils_proc_macros/src/lib.rs
@@ -20,7 +20,7 @@ pub fn tuple_ext_impl(_input: proc_macro::TokenStream) -> proc_macro::TokenStrea
         }
         use sealed::Sealed;
     ));
-    for i in 0..12 {
+    for i in 0..2 {
         generated.append_all(generate_sealed(i));
     }
     generated.into()
