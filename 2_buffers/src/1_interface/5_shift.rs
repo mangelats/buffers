4b436b9b8597b0321d7ed94e1b2390174cbe95c1
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-24 00:41:03 +0200

    TCR

--- 2_buffers/src/1_interface/5_shift.rs
+++ 2_buffers/src/1_interface/5_shift.rs
@@ -24,6 +24,7 @@ pub trait ShiftOneByOne: Buffer {}
 impl<T: ShiftOneByOne> BufferShift for T {
     unsafe fn shift_right<R: RangeBounds<usize>>(&mut self, to_move: R, positions: usize) {
         let (start, end) = start_end(self, to_move);
+
         let size = end - start;
         let new_end = end + positions;
 
@@ -39,7 +40,20 @@ impl<T: ShiftOneByOne> BufferShift for T {
     }
 
     unsafe fn shift_left<R: RangeBounds<usize>>(&mut self, to_move: R, positions: usize) {
-        todo!()
+        let (start, end) = start_end(self, to_move);
+
+        debug_assert!(start >= positions);
+
+        let size = end - start;
+        let new_start = start - positions;
+
+        for current in 0..size {
+            let new_pos = new_start + current;
+            let old_pos = start + current;
+            self.write_value(new_pos, self.read_value(old_pos));
+        }
+
+        // Old values left as is, since the bytes themselves are considered garbage
     }
 }
 
