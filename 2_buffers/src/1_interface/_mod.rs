eb6381bc35516ddfe7fa9a8832cc1d4014261ee7
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-24 00:26:28 +0200

    TCR

--- 2_buffers/src/1_interface/_mod.rs
+++ 2_buffers/src/1_interface/_mod.rs
@@ -10,7 +10,10 @@ pub mod ptrs;
 #[path = "4_refs.rs"]
 pub mod refs;
 
-#[path = "5_continuous_memory.rs"]
+#[path = "5_shift.rs"]
+pub mod shift;
+
+#[path = "6_continuous_memory.rs"]
 pub mod continuous_memory;
 
 pub use self::buffer::Buffer;
