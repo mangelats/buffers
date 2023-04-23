bfbf0e4483be78992f363e630c3df37c0fb46d0c
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-24 00:31:35 +0200

    TCR

---
+++ 2_buffers/src/1_interface/5_shift.rs
@@ -0,0 +1,20 @@
+use std::ops::RangeBounds;
+
+use super::Buffer;
+
+pub trait BufferShift: Buffer {
+    /// Shift a range of values to the right.
+    /// # Safety
+    /// The values must exist and the new location should be itself or an empty spot
+    ///
+    /// There should be enough space to the right
+    unsafe fn shift_right<R: RangeBounds<usize>>(&mut self, to_move: R, positions: usize);
+
+    /// Shift a range of values to the left.
+    ///
+    /// # Safety
+    /// The values must exist and the new location should be itself or an empty spot
+    ///
+    /// There should be enough space to the left
+    unsafe fn shift_left<R: RangeBounds<usize>>(&mut self, to_move: R, positions: usize);
+}
