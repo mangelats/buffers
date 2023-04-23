5c6cc02fa17c25bdbabbc8f4eb106ef5dfb5ca70
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-23 16:39:12 +0200

    TCR

--- 2_buffers/src/1_interface/3_continuous_memory.rs
+++ 2_buffers/src/1_interface/3_continuous_memory.rs
@@ -1,6 +1,19 @@
 use super::Buffer;
 
 pub trait ContinuousMemoryBuffer: Buffer {
+    /// Get a contant pointer to the value in the specified index.
+    ///
+    /// # SAFETY
+    /// `index` needs to be in bounds (`0 <= index < SIZE`). It's undefined behaviour when not.
+    ///
+    /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
     unsafe fn ptr(&self, index: usize) -> *const Self::Element;
+
+    /// Get a mutable pointer to the value in the specified index.
+    ///
+    /// # SAFETY
+    /// `index` needs to be in bounds (`0 <= index < SIZE`). It's undefined behaviour when not.
+    ///
+    /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
     unsafe fn mut_ptr(&mut self, index: usize) -> *mut Self::Element;
 }
