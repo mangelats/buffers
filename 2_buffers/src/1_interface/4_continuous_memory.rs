f54f24a096fb0d9fd39feab07094480856f2b99e
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-23 17:39:15 +0200

    TCR

--- 2_buffers/src/1_interface/4_continuous_memory.rs
+++ 2_buffers/src/1_interface/4_continuous_memory.rs
@@ -1,6 +1,7 @@
 use std::ops::Bound::*;
 use std::ops::RangeBounds;
 
+use super::ptrs::PtrBuffer;
 use super::Buffer;
 
 /// Trait for buffers which ensures that:
@@ -8,23 +9,7 @@ use super::Buffer;
 ///   2. All the memory is allocated continuously
 ///
 /// All common allocators actually fulfill this requirements but in some cases –like in a SoA– this may not be the case
-pub trait ContinuousMemoryBuffer: Buffer {
-    /// Get a contant pointer to the value in the specified index.
-    ///
-    /// # SAFETY
-    /// `index` needs to be in bounds (`0 <= index < capacity`). It's undefined behaviour when not.
-    ///
-    /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
-    unsafe fn ptr(&self, index: usize) -> *const Self::Element;
-
-    /// Get a mutable pointer to the value in the specified index.
-    ///
-    /// # SAFETY
-    /// `index` needs to be in bounds (`0 <= index < capacity`). It's undefined behaviour when not.
-    ///
-    /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
-    unsafe fn mut_ptr(&mut self, index: usize) -> *mut Self::Element;
-
+pub trait ContinuousMemoryBuffer: Buffer + PtrBuffer {
     /// Get the slice represanted by the range
     ///
     /// # SAFETY
