6efb89b37483cb88b97ab147f0846b7237c025db
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-23 16:37:12 +0200

    TCR

--- 2_buffers/src/1_interface/3_continuous_memory.rs
+++ 2_buffers/src/1_interface/3_continuous_memory.rs
@@ -1,3 +1,6 @@
 use super::Buffer;
 
-pub trait ContinuousMemoryBuffer: Buffer {}
+pub trait ContinuousMemoryBuffer: Buffer {
+    fn ptr(&self, index: usize) -> *const Self::Element;
+    fn mut_ptr(&mut self, index: usize) -> *mut Self::Element;
+}
