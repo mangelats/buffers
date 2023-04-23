4cfcce8662fe1052fa5421012196f37b3f1c7071
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-23 18:05:18 +0200

    TCR

--- 2_buffers/src/1_interface/4_refs.rs
+++ 2_buffers/src/1_interface/4_refs.rs
@@ -1,16 +1,23 @@
 use super::Buffer;
 
-/// Represents a buffer which has a reference
+/// Represents a buffer which has pointers to its elements
 ///
 /// Note that in some cases the elements themselves may not ahve a unique pointers (eg. zero-sized types)
 pub trait RefBuffer: Buffer {
+    type Constantreference<'a>
+    where
+        Self: 'a;
+    type MutableReference<'a>
+    where
+        Self: 'a;
+
     /// Get a contant pointer to the value in the specified index.
     ///
     /// # SAFETY
     /// `index` needs to be in bounds (`0 <= index < capacity`). It's undefined behaviour when not.
     ///
     /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
-    unsafe fn ptr(&self, index: usize) -> *const Self::Element;
+    unsafe fn index(&self, index: usize) -> Self::Constantreference<'_>;
 
     /// Get a mutable pointer to the value in the specified index.
     ///
@@ -18,5 +25,5 @@ pub trait RefBuffer: Buffer {
     /// `index` needs to be in bounds (`0 <= index < capacity`). It's undefined behaviour when not.
     ///
     /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
-    unsafe fn mut_ptr(&mut self, index: usize) -> *mut Self::Element;
+    unsafe fn mut_index(&mut self, index: usize) -> Self::MutableReference<'_>;
 }
