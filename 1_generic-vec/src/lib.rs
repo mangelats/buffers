aa491d50e2577d4f1e9d80bd2b8c372bf3bb7281
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-30 01:26:52 +0200

    TCR

--- 1_generic-vec/src/lib.rs
+++ 1_generic-vec/src/lib.rs
@@ -179,6 +179,21 @@ impl<T, B: Buffer<Element = T>> Vector<T, B> {
         self.len += 1;
     }
 
+    /// Removes and returns the element at position `index` within the vector,
+    /// shifting all elements after it to the left.
+    ///
+    /// Note: Because this shifts over the remaining elements, it has a
+    /// worst-case performance of *O*(*n*). If you don't need the order of elements
+    /// to be preserved, use [`swap_remove`] instead. If you'd like to remove
+    /// elements from the beginning of the `Vec`, consider using
+    /// [`VecDeque::pop_front`] instead.
+    ///
+    /// [`swap_remove`]: Vec::swap_remove
+    /// [`VecDeque::pop_front`]: crate::collections::VecDeque::pop_front
+    ///
+    /// # Panics
+    ///
+    /// Panics if `index` is out of bounds.
     pub fn remove(&mut self, index: usize) -> T {
         if index >= self.len {
             panic!("Index out of bounds")
