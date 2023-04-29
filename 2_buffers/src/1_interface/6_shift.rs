fe51699e1b2055e788a76319c8d684bfeb149580
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-30 00:31:54 +0200

    TCR

--- 2_buffers/src/1_interface/6_shift.rs
+++ 2_buffers/src/1_interface/6_shift.rs
@@ -14,7 +14,22 @@ pub trait BufferShift: Buffer {
     /// The values must exist and the new location should be itself or an empty spot
     ///
     /// There should be enough space to the right
-    unsafe fn shift_right<R: RangeBounds<usize>>(&mut self, to_move: R, positions: usize);
+    unsafe fn shift_right<R: RangeBounds<usize>>(&mut self, to_move: R, positions: usize) {
+        let (start, end) = start_end(self, to_move);
+
+        let size = end - start;
+        let new_end = end + positions;
+
+        debug_assert!(new_end < self.capacity());
+
+        for current in 0..size {
+            let new_pos = new_end - current;
+            let old_pos = end - current;
+            self.write_value(new_pos, self.read_value(old_pos));
+        }
+
+        // Old values left as is, since the bytes themselves are considered garbage
+    }
 
     /// Shift a range of values to the left.
     ///
