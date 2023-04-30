75743511fac244053d10b1a92ae3062f1099c770
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-04-30 14:01:40 +0200

    TCR

--- 1_generic-vec/src/lib.rs
+++ 1_generic-vec/src/lib.rs
@@ -224,6 +224,20 @@ impl<T, B: Buffer<Element = T>> Vector<T, B> {
     /// # Panics
     ///
     /// Panics if index is out of bounds.
+    ///
+    /// # Example
+    /// ```
+    /// # use buffers::base_buffers::heap::HeapBuffer;
+    /// # use generic_vec::Vector;
+    /// let mut vec = Vector::<u32, HeapBuffer<_>>::new();
+    /// vec.reserve(4);
+    /// vec.push(0);
+    /// vec.push(1);
+    /// vec.push(2);
+    /// vec.push(3);
+    ///
+    /// vec.swap_remove(1);
+    /// ```
     pub fn swap_remove(&mut self, index: usize) -> T {
         if index >= self.len {
             panic!("Index out of bounds")
