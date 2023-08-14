03eb8c9e72a2b7203e7c8b7de11c605966f7cf14
Martí Angelats i Ribera <marti.angelats@gmail.com>
2023-08-14 18:04:10 +0200

    Improve RefBuffer's docs

--- 1_buffers/src/1_interface/4_refs.rs
+++ 1_buffers/src/1_interface/4_refs.rs
@@ -1,33 +1,42 @@
 use super::{ptrs::PtrBuffer, Buffer};
 
-/// Represents a buffer which has pointers to its elements
+/// This trait extends the buffers that have the hability to generate references
+/// to an element in the buffer. This reference may be a regular rust reference
+/// (`&T`) but it doesn't have to. For example: in a structure of arrays setting
+/// it could be a structure of references.
 ///
-/// Note that in some cases the elements themselves may not ahve a unique pointers (eg. zero-sized types)
+/// If the buffer already implements [`PtrBuffer`] it may be able to be
+/// automatically implemented using [`DefaultRefBuffer`].
 pub trait RefBuffer: Buffer {
+    /// Type representing a reference to [`Buffer::Element`] with `'a` lifetime.
     type ConstantReference<'a>
     where
         Self: 'a;
+
+    /// Type representing a mutable reference to [`Buffer::Element`] with `'a`
+    /// lifetime.
     type MutableReference<'a>
     where
         Self: 'a;
 
-    /// Get a contant pointer to the value in the specified index.
+    /// Get a reference to the element in the specified position.
     ///
     /// # Safety
-    /// `index` needs to be in bounds (`0 <= index < capacity`). It's undefined behaviour when not.
-    ///
-    /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
+    ///   * `index` must be a valid position.
+    ///   * Position `index` must be filled.
     unsafe fn index(&self, index: usize) -> Self::ConstantReference<'_>;
 
-    /// Get a mutable pointer to the value in the specified index.
+    /// Get a mutable reference to the element in the specified position.
     ///
     /// # Safety
-    /// `index` needs to be in bounds (`0 <= index < capacity`). It's undefined behaviour when not.
-    ///
-    /// The pointer may point to unitialized or garbage data. It's the responsability of the caller to keep track of the state.
+    ///   * `index` must be a valid position.
+    ///   * Position `index` must be filled.
     unsafe fn mut_index(&mut self, index: usize) -> Self::MutableReference<'_>;
 }
 
+/// Helper trait which has a blanket implementation of [`RefBuffer`] for buffers
+/// which are [`PtrBuffer`] and its pointers are the regular rust ones
+/// (`*const T` and `*mut T`).
 pub trait DefaultRefBuffer:
     PtrBuffer<
     ConstantPointer = *const <Self as Buffer>::Element,
